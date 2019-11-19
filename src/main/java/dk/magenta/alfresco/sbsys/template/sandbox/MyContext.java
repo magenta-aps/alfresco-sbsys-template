6093a6049da6d992d479066771b5b25e4b8fac43
Andreas Kring <andreas@magenta.dk>
2019-11-19 11:59:16 +0100

    Updates sandbox docx template with Map

--- src/main/java/dk/magenta/alfresco/sbsys/template/sandbox/MyContext.java
+++ src/main/java/dk/magenta/alfresco/sbsys/template/sandbox/MyContext.java
@@ -1,13 +1,20 @@
 package dk.magenta.alfresco.sbsys.template.sandbox;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyContext {
 
     private String name;
     private String foo;
 
+    private Map<String, String> map = new HashMap<>();
+
     public MyContext(String name, String foo) {
         this.name = name;
         this.foo = foo;
+
+        map.put("xyz", "hurra3");
     }
 
     public String getName() {
@@ -24,5 +31,13 @@ public class MyContext {
 
     public void setFoo(String foo) {
         this.foo = foo;
+    }
+
+    public Map<String, String> getMap() {
+        return map;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
     }
 }
