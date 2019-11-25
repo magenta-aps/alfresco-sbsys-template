ba53c2e3f11710a0953f0fd7cca5e480f270c885
Andreas Kring <andreas@magenta.dk>
2019-11-25 15:16:33 +0100

    Added webscript stub for uploading documents to SBSYS

---
+++ src/main/java/dk/magenta/alfresco/sbsys/template/UploadDocument.java
@@ -0,0 +1,56 @@
+package dk.magenta.alfresco.sbsys.template;
+
+import org.alfresco.service.cmr.model.FileFolderService;
+import org.alfresco.service.cmr.repository.ContentService;
+import org.alfresco.service.cmr.site.SiteService;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class UploadDocument extends AbstractWebScript {
+
+    private ContentService contentService;
+    private FileFolderService fileFolderService;
+    private Properties properties;
+    private SiteService siteService;
+
+    @Override
+    public void execute(WebScriptRequest webScriptRequest, WebScriptResponse webScriptResponse) throws IOException {
+
+    }
+
+    public ContentService getContentService() {
+        return contentService;
+    }
+
+    public void setContentService(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    public FileFolderService getFileFolderService() {
+        return fileFolderService;
+    }
+
+    public void setFileFolderService(FileFolderService fileFolderService) {
+        this.fileFolderService = fileFolderService;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        this.properties = properties;
+    }
+
+    public SiteService getSiteService() {
+        return siteService;
+    }
+
+    public void setSiteService(SiteService siteService) {
+        this.siteService = siteService;
+    }
+}
