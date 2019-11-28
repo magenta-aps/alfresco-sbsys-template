df41d8204703ff74e6cce00d3493fc68abefe080
Andreas Kring <andreas@magenta.dk>
2019-11-28 14:36:41 +0100

    Initial commit

---
+++ src/test/java/dk/magenta/sbsys/template/multipart/MultipartFormDataRequesterApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.magenta.sbsys.template.multipart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipartFormDataRequesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
