8729024b85eb0509eb86da00b156112a69a792c0
Andreas Kring <andreas@magenta.dk>
2020-01-20 10:56:52 +0100

    Empty SBSYS upload behavior

---
+++ src/main/java/dk/magenta/alfresco/sbsys/template/behavior/SbsysUploadBehavior.java
@@ -0,0 +1,40 @@
+package dk.magenta.alfresco.sbsys.template.behavior;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.policy.Behaviour;
+import org.alfresco.repo.policy.JavaBehaviour;
+import org.alfresco.repo.policy.PolicyComponent;
+import org.alfresco.repo.version.VersionServicePolicies;
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.version.Version;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class SbsysUploadBehavior implements VersionServicePolicies.AfterCreateVersionPolicy {
+
+    private static Log logger = LogFactory.getLog(SbsysUploadBehavior.class);
+    private static final String AFTER_CREATE_VERSION = "afterCreateVersion";
+
+    private PolicyComponent policyComponent;
+
+    public void init() {
+        policyComponent.bindClassBehaviour(
+                VersionServicePolicies.AfterCreateVersionPolicy.QNAME,
+                ContentModel.ASPECT_VERSIONABLE,
+                new JavaBehaviour(
+                        this,
+                        AFTER_CREATE_VERSION,
+                        Behaviour.NotificationFrequency.TRANSACTION_COMMIT
+                )
+        );
+    }
+
+    @Override
+    public void afterCreateVersion(NodeRef versionableNode, Version version) {
+        logger.debug("Called behavior");
+    }
+
+    public void setPolicyComponent(PolicyComponent policyComponent) {
+        this.policyComponent = policyComponent;
+    }
+}
