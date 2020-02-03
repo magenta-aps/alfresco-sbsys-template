d2654329db8c7cf99110934998e9d4b691c34aef
Andreas Kring <andreas@magenta.dk>
2020-02-03 09:50:39 +0100

    Minor utils updates

--- utils/simulate_protocol.py
+++ utils/simulate_protocol.py
@@ -43,20 +43,20 @@ print()
 
 # Simulate SBSYS POSTing of the pre-uploaded document
 
-# input('Press enter to upload...')
-#
-# payload = {
-#     'preUploadId': pre_upload.get('preUploadId'),
-#     'token': {
-#         'token': get_token()
-#     }
-# }
-#
-# r = requests.post(
-#     ALF_UPLOAD_URL,
-#     json=payload,
-#     auth=admin_basic_auth
-# )
-#
-# print('POST ' + ALF_UPLOAD_URL)
-# print(r.status_code)
+input('Press enter to upload...')
+
+payload = {
+    'preUploadId': pre_upload.get('preUploadId'),
+    'token': {
+        'token': get_token()
+    }
+}
+
+r = requests.post(
+    ALF_UPLOAD_URL,
+    json=payload,
+    auth=admin_basic_auth
+)
+
+print('POST ' + ALF_UPLOAD_URL)
+print(r.status_code)
