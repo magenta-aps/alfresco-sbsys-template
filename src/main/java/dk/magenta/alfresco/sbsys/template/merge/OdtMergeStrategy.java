e35a947d958202a100912b318889b1936c62d11c
Andreas Kring <andreas@magenta.dk>
2020-03-06 11:03:57 +0100

    Minor refactoring

--- src/main/java/dk/magenta/alfresco/sbsys/template/merge/OdtMergeStrategy.java
+++ src/main/java/dk/magenta/alfresco/sbsys/template/merge/OdtMergeStrategy.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import org.alfresco.error.AlfrescoRuntimeException;
+import dk.magenta.alfresco.sbsys.template.json.Case;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -19,22 +19,7 @@ public class OdtMergeStrategy implements MergeStrategy {
 
         logger.debug("LoolMergeStrategy called");
 
-        Gson gson = new Gson();
-
-        JsonElement root = gson.toJsonTree(mergeDataWebscript.getSbsysCase());
-        Map<String, String> caseFields = new TreeMap<>();
-
-        if (root.isJsonObject()) {
-            JsonObject caseObj = root.getAsJsonObject();
-            Set<Map.Entry<String, JsonElement>> entries = caseObj.entrySet();
-            entries.forEach(entry -> {
-                List<String> keys = new ArrayList<>();
-                keys.add(entry.getKey());
-                extractCaseFields(entry.getValue(), caseFields, keys);
-            });
-        } else {
-            throw new AlfrescoRuntimeException("Merge error: root element not a JSON object");
-        }
+        Map<String, String> caseFields = flattenCase(mergeDataWebscript.getSbsysCase());
 
         caseFields.forEach((key, value) -> System.out.println(key + " " + value));
 
@@ -61,6 +46,23 @@ public class OdtMergeStrategy implements MergeStrategy {
             }
         }
 
-        keys.remove(keys.size() - 1);
+        if (!keys.isEmpty()) {
+            keys.remove(keys.size() - 1);
+        }
+    }
+
+    /**
+     * Convert an SBSYS case (JSON) object representation into a flat map of
+     * keys and values
+     */
+    private Map<String, String> flattenCase(Case sbsysCase) {
+        Gson gson = new Gson();
+
+        JsonElement root = gson.toJsonTree(sbsysCase);
+        Map<String, String> caseFields = new TreeMap<>();
+        List<String> keys = new ArrayList<>();
+        extractCaseFields(root, caseFields, keys);
+
+        return caseFields;
     }
 }
