1550d29dc78f14aa2d3ee1e8aff7e3964172ae1d
Andreas Kring <andreas@magenta.dk>
2020-03-17 10:56:09 +0100

    Added ODT initialization

--- utils/init_system.sh
+++ utils/init_system.sh
@@ -4,9 +4,12 @@ curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/sws
 curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/preview
 curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/edit
 curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon2.docx
+curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon3.docx
+curl -i -u admin:admin -X DELETE http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon5.odt
 
 curl -i -u admin:admin -X MKCOL http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/pre-upload
 curl -i -u admin:admin -X MKCOL http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/preview
 curl -i -u admin:admin -X MKCOL http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/edit
 curl -i -u admin:admin -T '../src/main/resources/sandbox/skabelon2.docx' "http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon2.docx"
 curl -i -u admin:admin -T '../src/main/resources/sandbox/skabelon3.docx' "http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon3.docx"
+curl -i -u admin:admin -T '../src/main/resources/sandbox/skabelon5.odt' "http://localhost:8080/alfresco/webdav/Sites/swsdp/documentLibrary/skabelon5.odt"
